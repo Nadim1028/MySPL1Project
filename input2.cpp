0117f1f1b11b5e0ebcb62b90159d000107c2c450
Md Nadim Ahmed <46520543+Nadim1028@users.noreply.github.com>
2019-03-14 03:35:04 +0600

    Input for Similarity4.cpp

---
+++ input2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int sum,b,c,i;
+    sum=b+c;
+
+    for(i=0;i<10;i++)
+    {
+        cout << i*i << endl;
+    }
+    return 0;
+}
---
+++ input3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int add,y,z,j;
+
+    add=y+z;
+
+    for(j=0;j<10;j++)
+    {
+        cout << j*j << endl;
+    }
+    return 0;
+}
